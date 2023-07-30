81090e405fc555d94205e4294dbdac5ff29214a3
lammiejay <lammiejay02@gmail.com>
2023-07-30 13:42:10 +0100

    rectified error

--- JS/forecast.js
+++ JS/forecast.js
@@ -1,1 +1,15 @@
 const key = 'h15ATrTrKB9NZz9gGEMhbgU0rP4FYJ9b';
+
+const getCity = async (city) => {
+
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const query = `?apikey=${key}&q=${city}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    console.log(data);
+
+};
+
+getCity('barcelona');
